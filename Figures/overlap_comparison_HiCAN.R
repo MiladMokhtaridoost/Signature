f825749738b5c0d750995d135eff624a2aa9ddd2
Philipp Maass Lab <129893570+MaassLab@users.noreply.github.com>
2024-02-01 11:55:59 -0500

    Add files via upload

---
+++ Figures/overlap_comparison_HiCAN.R
@@ -0,0 +1,416 @@
+################################################################################
+# Venn Diagram for HiCAN and Signature (all cell types grouped, not cell-type-
+# specific analysis)
+#
+# Developed by: Jordan Chalmers
+#
+#
+################################################################################
+options(scipen=999)
+suppressMessages({
+  library(ggplot2)
+  library(dplyr)
+  library(tidyr)
+  library(openxlsx, lib="Z:/Signature/pipeline/signature/scr/Rlib-4.2.1")
+  library(ggVennDiagram, lib="Z:/Signature/pipeline/signature/scr/Rlib-4.2.1")
+  library(VennDiagram, lib="Z:/Signature/pipeline/signature/scr/Rlib-4.2.1")
+})
+cat(date(),"\n-----------------------------\n")
+
+
+
+#_____Prepare data______________________________________________________________
+
+# load data
+hican_nhub <- read.table("Z:/Jordan/signature/revisions/HiCAN/eight_cells_Nbasis_top100.txt", header = T)
+hican_shub <- read.table("Z:/Jordan/signature/revisions/HiCAN/eight_cells_Sbasis_top100.txt", header = T)
+sign_all <- read.table("Z:/Signature/results/merged_output/diploid/signature_trans1vsAll_1MB_merged_qvalue_pos.txt", header = T)
+sign_GM <- read.table("Z:/Jordan/signature/revisions/Signature_disease/output/GM12878_Arima.GM12878_Rao/GM12878_Arima.GM12878_Rao.trans1vsAll.1MB.qvalue.pos.txt", header = T)
+
+# filter to appropriate cell types
+hican_nhub <- hican_nhub %>% select(-c("GM23248", "WI38_raf"))
+hican_shub <- hican_shub %>% select(-c("GM23248", "WI38_raf"))
+sign_all <- sign_all[,c(1, grep("IMR90", colnames(sign_all)), grep("HMEC", colnames(sign_all)), grep("HUVEC", colnames(sign_all)), grep("NHEK", colnames(sign_all)), grep("HAEC", colnames(sign_all)))]
+sign <- merge(x = sign_all, y = sign_GM, by = "ID", all = TRUE)
+# # keep only Rao data (or one of each cell type)
+# sign <- sign[,c(1,3,4,5,7,9,11)]
+
+# split up interaction ID information into new columns
+sign$ID <- sub("B", "\\.B", as.character(sign$ID))
+sign <- sign %>% separate(ID, sep = "\\.", into = c("chrA", "st1", "end1","chrB","st2","end2"), remove = FALSE)
+sign$chrA <- gsub("A", "", sign$chrA)
+sign$chrB <- gsub("B", "", sign$chrB)
+
+
+
+#_____Convert HiCAN res to Signature res________________________________________
+# rationale: only start OR end will match signature bin, so if divided by 1Mb and we get a decimal, it means it's the bin that needs to be converted
+# so if start has decimal, you need to round down to the closest 1MB, if end has decimal, you need to round up
+
+# convert hican_shub #
+df <- hican_shub
+
+for (ct in colnames(hican_shub)){
+  
+  df <- df %>% separate(ct, sep = "\\.", into = c("chrA", "st1", "end1"), remove = FALSE)
+  
+  # convert
+  for (r in 1:nrow(df)) {
+    
+    if (grepl("\\.",as.numeric(df$st1[r])/1000000)){
+      
+      df$st1[r] <- floor(as.numeric(df$st1[r])/1000000)*1000000
+      
+    } else if (grepl("\\.",as.numeric(df$end1[r])/1000000)){
+      
+      df$end1[r] <- ceiling(as.numeric(df$end1[r])/1000000)*1000000
+      
+    }
+    
+  }
+  
+  # replace 500kb loci with new 1mb loci
+  coln <- grep(ct, colnames(df))
+  df[coln] <- paste(df$chrA, df$st1, df$end1, sep = ".")
+  
+  # remove these columns for next cell type
+  df <- df %>% select(-c("chrA", "st1", "end1"))
+
+}
+
+hican_shub <- df
+rm(df)
+
+
+# convert hican_nhub #
+df <- hican_nhub
+
+for (ct in colnames(hican_nhub)){
+  
+  df <- df %>% separate(ct, sep = "\\.", into = c("chrA", "st1", "end1"), remove = FALSE)
+  
+  # convert
+  for (r in 1:nrow(df)) {
+    
+    if (grepl("\\.",as.numeric(df$st1[r])/1000000)){
+      
+      df$st1[r] <- floor(as.numeric(df$st1[r])/1000000)*1000000
+      
+    } else if (grepl("\\.",as.numeric(df$end1[r])/1000000)){
+      
+      df$end1[r] <- ceiling(as.numeric(df$end1[r])/1000000)*1000000
+      
+    }
+    
+  }
+  
+  # replace 500kb loci with new 1mb loci
+  coln <- grep(ct, colnames(df))
+  df[coln] <- paste(df$chrA, df$st1, df$end1, sep = ".")
+  
+  # remove these columns for next cell type
+  df <- df %>% select(-c("chrA", "st1", "end1"))
+  
+}
+
+hican_nhub <- df
+rm(df)
+
+
+
+#_____Create interaction loci lists_____________________________________________
+
+# HiCAN speckle
+hican_shub_list <- unique(c(hican_shub[,1],hican_shub[,2],hican_shub[,3],hican_shub[,4],hican_shub[,5],hican_shub[,6]))
+
+# HiCAN nucleolus
+hican_nhub_list <- unique(c(hican_nhub[,1],hican_nhub[,2],hican_nhub[,3],hican_nhub[,4],hican_nhub[,5],hican_nhub[,6]))
+
+# Signature
+sign <- sign[!rowSums(is.na(sign)) == length(colnames(sign))-7, ]   # remove interactions with only NAs (first 7 columns are related to ID)
+sign$bin1 <- paste(sign$chrA, sign$st1, sign$end1, sep = ".")
+sign$bin2 <- paste(sign$chrB, sign$st2, sign$end2, sep = ".")
+sign_list <- c(sign$bin1,sign$bin2)
+sign_list <- unique(sign_list)
+
+
+
+#_____Produce Venn Diagram______________________________________________________
+
+gglist <- list("signature" = sign_list,
+               "hican_speckle" = hican_shub_list,
+               "hican_nucleolus" = hican_nhub_list)
+
+
+vd <- ggVennDiagram(gglist, set_size = 3, label_alpha = 0, label_size = 4, label = "count") +
+              labs(title = "Signature (q<0.05) vs HiCAN", fill = "1MB loci") +
+              scale_fill_distiller(palette = "Reds", direction = 1) +
+              scale_color_brewer(palette = "Greys") +
+              theme(plot.title = element_text(face="bold", colour="black", size=14, hjust=0.5, vjust=3),
+                    plot.subtitle = element_text(face="plain", colour="black", size=12, hjust=0.5),
+                    legend.title = element_text(face="plain", colour="black", size=10, hjust=0.5),
+                    legend.text = element_text(face="plain", colour="black", size=8),
+                    legend.position = NULL)
+
+ggsave("Z:/Jordan/signature/revisions/HiCAN/signature_vs_HiCAN_VD.pdf", width = 8, height = 6)
+
+
+# get list of unique bins to extract genes later
+all_overlaps <- calculate.overlap(gglist) 
+
+total <- lengths(all_overlaps)
+#index 1: a5 = all
+#index 2: a2 = sp+si
+#index 3: a4 = nu+si
+#index 4: a6 = sp+nu
+#index 5: a1 = si
+#index 6: a3 = sp
+#index 7: a7 = nu
+
+ggg <- c(rep("all",total[1]),
+         rep("sp+si",total[2]),
+         rep("nu+si",total[3]),
+         rep("sp+nu",total[4]),
+         rep("si",total[5]),
+         rep("sp",total[6]),
+         rep("nu",total[7]))
+
+bbb <- c(all_overlaps[[1]],
+         all_overlaps[[2]],
+         all_overlaps[[3]],
+         all_overlaps[[4]],
+         all_overlaps[[5]],
+         all_overlaps[[6]],
+         all_overlaps[[7]])
+
+genes_df <- data.frame("bins" = bbb,
+                       "group" = ggg)
+
+# # export
+# write.table(genes_df, "Z:/Jordan/signature/revisions/HiCAN/full_overlap_list.txt", sep = "\t", quote = FALSE, row.names = FALSE, col.names = TRUE)
+
+
+
+#_____Annotate with genes_______________________________________________________
+
+dat <- read.table("Z:/Jordan/signature/CD_gtex/output/annotated_geneIDs_trans1MB_all_bins.txt", sep = "\t", header = TRUE)
+
+dat$bin_ID <- gsub(":",".",dat$bin_ID)
+dat$bin_ID <- gsub("-",".",dat$bin_ID)
+
+genes_df$gene_IDs <- "NA"
+
+for (b in 1:nrow(genes_df)){
+  
+  gbi <- which(dat$bin_ID == genes_df$bins[b])
+  
+  if(length(gbi) > 0){genes_df$gene_IDs[b] <- dat$gene_IDs[gbi]}
+
+}
+
+# export
+write.table(genes_df, "Z:/Jordan/signature/revisions/HiCAN/full_overlap_list_annotated_geneIDs.txt", sep = "\t", quote = FALSE, row.names = FALSE, col.names = TRUE)
+
+
+
+
+################################################################################
+#     Repeat analysis for q<0.0001
+################################################################################
+rm(list = ls()) # clear environment
+
+#_____Prepare data______________________________________________________________
+
+# load data
+hican_nhub <- read.table("Z:/Jordan/signature/revisions/HiCAN/eight_cells_Nbasis_top100.txt", header = T)
+hican_shub <- read.table("Z:/Jordan/signature/revisions/HiCAN/eight_cells_Sbasis_top100.txt", header = T)
+sign_all <- read.table("Z:/Signature/results/merged_output/diploid/signature_trans1vsAll_1MB_merged_qvalue_pos.txt", header = T)
+sign_GM <- read.table("Z:/Jordan/signature/revisions/Signature_disease/output/GM12878_Arima.GM12878_Rao/GM12878_Arima.GM12878_Rao.trans1vsAll.1MB.qvalue.pos.txt", header = T)
+
+# filter to appropriate cell types
+hican_nhub <- hican_nhub %>% select(-c("GM23248", "WI38_raf"))
+hican_shub <- hican_shub %>% select(-c("GM23248", "WI38_raf"))
+sign_all <- sign_all[,c(1, grep("IMR90", colnames(sign_all)), grep("HMEC", colnames(sign_all)), grep("HUVEC", colnames(sign_all)), grep("NHEK", colnames(sign_all)), grep("HAEC", colnames(sign_all)))]
+sign <- merge(x = sign_all, y = sign_GM, by = "ID", all = TRUE)
+sign <- sign[!rowSums(is.na(sign)) == length(colnames(sign))-1, ]   # remove interactions with only NAs (first column is ID)
+
+# filter to q < 0.01
+for (c in 2:ncol(sign)){
+  
+  index <- which(sign[,c] > 0.0001)
+  sign[index,c] <- NA
+  
+}
+sign <- sign[!rowSums(is.na(sign)) == length(colnames(sign))-1, ]   # remove interactions with only NAs (first column is ID)
+
+
+# split up interaction ID information into new columns
+sign$ID <- sub("B", "\\.B", as.character(sign$ID))
+sign <- sign %>% separate(ID, sep = "\\.", into = c("chrA", "st1", "end1","chrB","st2","end2"), remove = FALSE)
+sign$chrA <- gsub("A", "", sign$chrA)
+sign$chrB <- gsub("B", "", sign$chrB)
+
+
+
+#_____Convert HiCAN res to Signature res________________________________________
+# rationale: only start OR end will match signature bin, so if divided by 1Mb and we get a decimal, it means it's the bin that needs to be converted
+# so if start has decimal, you need to round down to the closest 1MB, if end has decimal, you need to round up
+
+# convert hican_shub #
+df <- hican_shub
+
+for (ct in colnames(hican_shub)){
+  
+  df <- df %>% separate(ct, sep = "\\.", into = c("chrA", "st1", "end1"), remove = FALSE)
+  
+  # convert
+  for (r in 1:nrow(df)) {
+    
+    if (grepl("\\.",as.numeric(df$st1[r])/1000000)){
+      
+      df$st1[r] <- floor(as.numeric(df$st1[r])/1000000)*1000000
+      
+    } else if (grepl("\\.",as.numeric(df$end1[r])/1000000)){
+      
+      df$end1[r] <- ceiling(as.numeric(df$end1[r])/1000000)*1000000
+      
+    }
+    
+  }
+  
+  # replace 500kb loci with new 1mb loci
+  coln <- grep(ct, colnames(df))
+  df[coln] <- paste(df$chrA, df$st1, df$end1, sep = ".")
+  
+  # remove these columns for next cell type
+  df <- df %>% select(-c("chrA", "st1", "end1"))
+  
+}
+
+hican_shub <- df
+rm(df)
+
+
+# convert hican_nhub #
+df <- hican_nhub
+
+for (ct in colnames(hican_nhub)){
+  
+  df <- df %>% separate(ct, sep = "\\.", into = c("chrA", "st1", "end1"), remove = FALSE)
+  
+  # convert
+  for (r in 1:nrow(df)) {
+    
+    if (grepl("\\.",as.numeric(df$st1[r])/1000000)){
+      
+      df$st1[r] <- floor(as.numeric(df$st1[r])/1000000)*1000000
+      
+    } else if (grepl("\\.",as.numeric(df$end1[r])/1000000)){
+      
+      df$end1[r] <- ceiling(as.numeric(df$end1[r])/1000000)*1000000
+      
+    }
+    
+  }
+  
+  # replace 500kb loci with new 1mb loci
+  coln <- grep(ct, colnames(df))
+  df[coln] <- paste(df$chrA, df$st1, df$end1, sep = ".")
+  
+  # remove these columns for next cell type
+  df <- df %>% select(-c("chrA", "st1", "end1"))
+  
+}
+
+hican_nhub <- df
+rm(df)
+
+
+
+#_____Create interaction loci lists_____________________________________________
+
+# HiCAN speckle
+hican_shub_list <- unique(c(hican_shub[,1],hican_shub[,2],hican_shub[,3],hican_shub[,4],hican_shub[,5],hican_shub[,6]))
+
+# HiCAN nucleolus
+hican_nhub_list <- unique(c(hican_nhub[,1],hican_nhub[,2],hican_nhub[,3],hican_nhub[,4],hican_nhub[,5],hican_nhub[,6]))
+
+# Signature
+sign$bin1 <- paste(sign$chrA, sign$st1, sign$end1, sep = ".")
+sign$bin2 <- paste(sign$chrB, sign$st2, sign$end2, sep = ".")
+sign_list <- c(sign$bin1,sign$bin2)
+sign_list <- unique(sign_list)
+
+
+
+#_____Produce Venn Diagram______________________________________________________
+
+gglist <- list("signature" = sign_list,
+               "hican_speckle" = hican_shub_list,
+               "hican_nucleolus" = hican_nhub_list)
+
+
+vd <- ggVennDiagram(gglist, set_size = 3, label_alpha = 0, label_size = 4, label = "count") +
+  labs(title = "Signature (q<0.0001) vs HiCAN", fill = "1MB loci") +
+  scale_fill_distiller(palette = "Reds", direction = 1) +
+  scale_color_brewer(palette = "Greys") +
+  theme(plot.title = element_text(face="bold", colour="black", size=14, hjust=0.5, vjust=3),
+        plot.subtitle = element_text(face="plain", colour="black", size=12, hjust=0.5),
+        legend.title = element_text(face="plain", colour="black", size=10, hjust=0.5),
+        legend.text = element_text(face="plain", colour="black", size=8),
+        legend.position = NULL)
+
+ggsave("Z:/Jordan/signature/revisions/HiCAN/output/signature_vs_HiCAN_VD.0001.pdf", width = 8, height = 6)
+
+
+# get list of unique bins to extract genes later
+all_overlaps <- calculate.overlap(gglist) 
+
+total <- lengths(all_overlaps)
+#index 1: a5 = all
+#index 2: a2 = sp+si
+#index 3: a4 = nu+si
+#index 4: a6 = sp+nu
+#index 5: a1 = si
+#index 6: a3 = sp
+#index 7: a7 = nu
+
+ggg <- c(rep("all",total[1]),
+         rep("sp+si",total[2]),
+         rep("nu+si",total[3]),
+         rep("sp+nu",total[4]),
+         rep("si",total[5]),
+         rep("sp",total[6]),
+         rep("nu",total[7]))
+
+bbb <- c(all_overlaps[[1]],
+         all_overlaps[[2]],
+         all_overlaps[[3]],
+         all_overlaps[[4]],
+         all_overlaps[[5]],
+         all_overlaps[[6]],
+         all_overlaps[[7]])
+
+genes_df <- data.frame("bins" = bbb,
+                       "group" = ggg)
+
+
+#_____Annotate with genes_______________________________________________________
+
+dat <- read.table("Z:/Jordan/signature/CD_gtex/output/annotated_geneIDs_trans1MB_all_bins.txt", sep = "\t", header = TRUE)
+
+dat$bin_ID <- gsub(":",".",dat$bin_ID)
+dat$bin_ID <- gsub("-",".",dat$bin_ID)
+
+genes_df$gene_IDs <- "NA"
+
+for (b in 1:nrow(genes_df)){
+  
+  gbi <- which(dat$bin_ID == genes_df$bins[b])
+  
+  if(length(gbi) > 0){genes_df$gene_IDs[b] <- dat$gene_IDs[gbi]}
+  
+}
+
+# export
+write.table(genes_df, "Z:/Jordan/signature/revisions/HiCAN/output/full_overlap_list_annotated_geneIDs.0001.txt", sep = "\t", quote = FALSE, row.names = FALSE, col.names = TRUE)
