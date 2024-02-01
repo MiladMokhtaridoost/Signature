23e6f0bd34b978650fb6fcf9dde13404fffddb1f
Philipp Maass Lab <129893570+MaassLab@users.noreply.github.com>
2024-02-01 12:11:49 -0500

    Add files via upload

---
+++ Figures/overlap_comparison_GM.R
@@ -0,0 +1,140 @@
+################################################################################
+# VennDiagram for SPRITE's and Signature's GM12878
+#
+# Developed by: Jordan Chalmers
+#
+#
+################################################################################
+options(scipen=999)
+suppressMessages({
+  library(ggplot2)
+  library(dplyr)
+  library(tidyr)
+  library(openxlsx, lib="/hpf/largeprojects/pmaass/Signature/pipeline/signature/scr/Rlib-4.2.1")
+  library(ggVennDiagram, lib="/hpf/largeprojects/pmaass/Signature/pipeline/signature/scr/Rlib-4.2.1")
+  library(VennDiagram, lib="/hpf/largeprojects/pmaass/Signature/pipeline/signature/scr/Rlib-4.2.1")
+})
+cat(date(),"\n-----------------------------\n")
+
+
+#_____Prepare data______________________________________________________________
+
+# load data
+sprite_nhub <- read.xlsx("/hpf/largeprojects/pmaass/Jordan/signature/revisions/sprite/mmc2.xlsx", sheet = "2D_nucleolar_hub_GM12878")
+sprite_ahub <- read.xlsx("/hpf/largeprojects/pmaass/Jordan/signature/revisions/sprite/mmc2.xlsx", sheet = "2E_active_hub_GM12878")
+sign_all <- read.table("/hpf/largeprojects/pmaass/Signature/results/merged_output/diploid/signature_trans1vsAll_1MB_merged_qvalue_pos.txt", header = T)
+sign_GM <- read.table("/hpf/largeprojects/pmaass/Jordan/signature/revisions/Signature_disease/output/GM12878_Arima.GM12878_Rao/GM12878_Arima.GM12878_Rao.trans1vsAll.1MB.qvalue.pos.txt", header = T)
+
+# split up interaction ID information into new columns
+colnm <- c("chrA", "st1", "end1","chrB","st2","end2")
+
+sign_all <- sign_all %>% select(ID)
+sign_all$ID <- sub("B", "\\.B", as.character(sign_all$ID))
+sign_all <- sign_all %>% separate(ID, sep = "\\.", into = colnm, remove = FALSE)
+sign_all$chrA <- gsub("A", "", sign_all$chrA)
+sign_all$chrB <- gsub("B", "", sign_all$chrB)
+
+sign_GM <- sign_GM %>% select(ID)
+sign_GM$ID <- sub("B", "\\.B", as.character(sign_GM$ID))
+sign_GM <- sign_GM %>% separate(ID, sep = "\\.", into = colnm, remove = FALSE)
+sign_GM$chrA <- gsub("A", "", sign_GM$chrA)
+sign_GM$chrB <- gsub("B", "", sign_GM$chrB)
+
+# prepare list of bins group A (signature all)
+sign_all$bin1 <- paste(sign_all$chrA, sign_all$st1, sign_all$end1, sep = ".")
+sign_all$bin2 <- paste(sign_all$chrB, sign_all$st2, sign_all$end2, sep = ".")
+sign_all_list <- c(sign_all$bin1,sign_all$bin2)
+sign_all_list <- unique(sign_all_list)
+
+# prepare list of bins group B (signature GM)
+sign_GM$bin1 <- paste(sign_GM$chrA, sign_GM$st1, sign_GM$end1, sep = ".")
+sign_GM$bin2 <- paste(sign_GM$chrB, sign_GM$st2, sign_GM$end2, sep = ".")
+sign_GM_list <- c(sign_GM$bin1,sign_GM$bin2)
+sign_GM_list <- unique(sign_GM_list)
+
+# prepare list of bins group C (SPRITE GM-a)
+sprite_ahub$bin <- paste(sprite_ahub$chr, sprite_ahub$st, sprite_ahub$end, sep = ".")
+sprite_ahub_list <- unique(sprite_ahub$bin)
+
+# prepare list of bins group D (SPRITE GM-n)
+sprite_nhub$bin <- paste(sprite_nhub$chr, sprite_nhub$st, sprite_nhub$end, sep = ".")
+sprite_nhub_list <- unique(sprite_nhub$bin)
+
+
+
+#_____Produce Venn Diagram______________________________________________________
+
+gglist <- list("sign_GM" = sign_GM_list,
+               "SPRITE_nucHub" = sprite_ahub_list,
+               "SPRITE_actHub" = sprite_nhub_list)
+
+ggVennDiagram(gglist, set_size = 3, label_alpha = 0, label_size = 4) +
+              labs(title = "Signature vs SPRITE (q<0.05) [GM12878]", fill = "1MB loci") +
+              scale_fill_distiller(palette = "Reds", direction = 1) +
+              scale_color_brewer(palette = "Greys") +
+              theme(plot.title = element_text(face="bold", colour="black", size=14, hjust=0.5, vjust=3),
+                    plot.subtitle = element_text(face="plain", colour="black", size=12, hjust=0.5),
+                    legend.title = element_text(face="plain", colour="black", size=10, hjust=0.5),
+                    legend.text = element_text(face="plain", colour="black", size=8),
+                    legend.position = NULL)
+
+ggsave("/hpf/largeprojects/pmaass/Jordan/signature/revisions/sprite/output/signature_vs_sprite_VD.pdf", width = 8, height = 6)
+
+
+# get list of unique bins to extract genes later
+all_overlaps <- calculate.overlap(gglist) 
+
+total <- lengths(all_overlaps)
+#index 1: a5 = all
+#index 2: a2 = nu+si
+#index 3: a4 = ac+si
+#index 4: a6 = ac+nu
+#index 5: a1 = si
+#index 6: a3 = nu
+#index 7: a7 = ac
+
+ggg <- c(rep("all",total[1]),
+         rep("nu+si",total[2]),
+         rep("ac+si",total[3]),
+         rep("ac+nu",total[4]),
+         rep("si",total[5]),
+         rep("nu",total[6]),
+         rep("ac",total[7]))
+
+bbb <- c(all_overlaps[[1]],
+         all_overlaps[[2]],
+         all_overlaps[[3]],
+         all_overlaps[[4]],
+         all_overlaps[[5]],
+         all_overlaps[[6]],
+         all_overlaps[[7]])
+
+genes_df <- data.frame("bins" = bbb,
+                       "group" = ggg)
+
+# export
+write.table(genes_df, "/hpf/largeprojects/pmaass/Jordan/signature/revisions/sprite/output/full_overlap_list.txt", sep = "\t", quote = FALSE, row.names = FALSE, col.names = TRUE)
+
+
+
+#_____Annotate with genes_______________________________________________________
+
+dat <- read.table("/hpf/largeprojects/pmaass/Jordan/signature/CD_gtex/output/annotated_geneIDs_trans1MB_all_bins.txt", header = T, sep = "\t")
+
+dat$bin_ID <- gsub(":",".",dat$bin_ID)
+dat$bin_ID <- gsub("-",".",dat$bin_ID)
+
+genes_df$gene_IDs <- "NA"
+
+for (b in 1:nrow(genes_df)){
+  
+  gbi <- which(dat$bin_ID == genes_df$bins[b])
+  
+  if(length(gbi) > 0){genes_df$gene_IDs[b] <- dat$gene_IDs[gbi]}
+  
+}
+
+# export
+write.table(genes_df, "/hpf/largeprojects/pmaass/Jordan/signature/revisions/sprite/output/full_overlap_list_annotated_geneIDs.txt", sep = "\t", quote = FALSE, row.names = FALSE, col.names = TRUE)
+
+
